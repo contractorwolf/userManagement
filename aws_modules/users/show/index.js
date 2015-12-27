a2f65fea15ddacd9a6b1a479d3df67e7f0e1edd9
contractorwolf <contractorwolf@hotmail.com>
2015-12-27 12:06:24 +0100

    show now pulls from the db

--- aws_modules/users/show/index.js
+++ aws_modules/users/show/index.js
@@ -2,12 +2,35 @@
  * AWS Module: Action: Modularized Code
  */
 
-// Export For Lambda Handler
+var AWS = require("aws-sdk");
+var dynamodbDoc = new AWS.DynamoDB.DocumentClient({region: "us-east-1"});
+
+const tableName = process.env.TABLE_NAME + "-" + process.env.JAWS_STAGE;
+
+// Export for Lambda Handler
 module.exports.run = function(event, context, cb) {
-  return cb(null, action());
+  	try {
+	    action(event.username,cb);
+	} catch (error) {
+		cb("Caught: " + error);
+	}
 };
 
-// Your Code
-var action = function() {
-  return { users: 'show all users' };
+
+// Your code
+var action = function(username,cb) {
+	var params = {
+    	TableName: tableName
+	};
+
+
+	var response = dynamodbDoc.scan(params, function(err, data) {
+	  	if (err) {
+	    	cb("Error in query: " + err);
+	  	} else {
+	    	cb(null,data);
+	  	}
+	});
 };
+
+
